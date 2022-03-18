63e081d6105d3a559fe29fe4ff04d43801de004a
Plutoyxt <88093829+Plutoyxt@users.noreply.github.com>
2022-03-18 20:51:40 +0800

    Update __init__.py

--- BboxToolkit/datasets/__init__.py
+++ BboxToolkit/datasets/__init__.py
@@ -4,6 +4,7 @@ from .misc import (img_exts, get_classes, change_cls_order, merge_prior_contents
 from .io import load_imgs, load_pkl, save_pkl
 from .DOTAio import load_dota, load_dota_submission, save_dota_submission
 from .DIORio import load_dior_hbb, load_dior_obb, load_dior
+from .SSDDio import load_dior_hbb, load_dior_obb, load_dior
 from .HRSCio import load_hrsc
 from .MSRA_TD500io import load_msra_td500
 from .HUST_TR400io import load_hust_tr400
